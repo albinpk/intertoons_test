8c288afa0178df028a9b9742c2e094654d318392
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 21:09:44 +0530

    feat: create appBar with delivery address

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -12,6 +12,39 @@ class HomeView extends StatelessWidget {
     return SafeArea(
       child: CustomScrollView(
         slivers: [
+          // Delivery address
+          SliverAppBar(
+            toolbarHeight: kToolbarHeight + 20,
+            floating: true,
+            snap: true,
+            backgroundColor: Colors.white,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  'Deliver to',
+                  style: TextStyle(color: Colors.black87),
+                ),
+                const SizedBox(height: 5),
+                Text(
+                  'The delivery address goes here.',
+                  style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                        color: Colors.black54,
+                      ),
+                ),
+                const SizedBox(height: 5),
+                SizedBox(
+                  width: 100,
+                  height: 5,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(10),
+                    child: const ColoredBox(color: Colors.red),
+                  ),
+                ),
+              ],
+            ),
+          ),
+
           // Slider banners page view
           SliverToBoxAdapter(
             child: SizedBox(
