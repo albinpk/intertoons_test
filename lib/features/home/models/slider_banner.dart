99899bcf0c553b84ac13cda10a8496d57c46b589
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 12:17:24 +0530

    refactor: create `SliderBanner` model

---
+++ lib/features/home/models/slider_banner.dart
@@ -0,0 +1,17 @@
+/// Slider banner model
+class SliderBanner {
+  const SliderBanner({
+    required this.id,
+    required this.bannerOrder,
+    required this.bannerImg,
+  });
+
+  /// Banner id.
+  final int id;
+
+  /// Banner order.
+  final String bannerOrder;
+
+  /// Banner image.
+  final String bannerImg;
+}
