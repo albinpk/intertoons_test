92a3b0165f7a61bcd49e918af7927231f2a78478
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 13:58:52 +0530

    feat: create `FeaturedProduct` class

---
+++ lib/features/home/models/featured_product.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+import 'product_base.dart';
+
+/// Featured product model.
+class FeaturedProduct extends ProductBase {
+  const FeaturedProduct({
+    required super.id,
+    required super.name,
+    required super.sku,
+    required super.categoryId,
+    required super.categoryName,
+    required super.isVeg,
+    required super.description,
+    required super.price,
+    required super.imageUrl,
+    required super.variations,
+    required this.specialPrice,
+    required this.availableFrom,
+    required this.availableTo,
+  });
+
+  /// Product special price.
+  final double specialPrice;
+
+  /// The time this featured product starts selling.
+  ///
+  /// see [availableTo].
+  final TimeOfDay availableFrom;
+
+  /// The time this featured product stops selling.
+  ///
+  /// see [availableFrom].
+  final TimeOfDay availableTo;
+}
