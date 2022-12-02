428a03af18bcd759cc6de0739164dbadccad27c7
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 17:20:43 +0530

    refactor: add JSON serialization in `SliderBanner` class

--- lib/features/home/models/slider_banner.dart
+++ lib/features/home/models/slider_banner.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Slider banner model
 class SliderBanner {
   const SliderBanner({
@@ -14,4 +16,25 @@ class SliderBanner {
 
   /// Banner image.
   final String bannerImg;
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'id': id,
+      'banner_order': bannerOrder,
+      'banner_img': bannerImg,
+    };
+  }
+
+  factory SliderBanner.fromMap(Map<String, dynamic> map) {
+    return SliderBanner(
+      id: map['id'] as int,
+      bannerOrder: map['banner_order'] as String,
+      bannerImg: map['banner_img'] as String,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory SliderBanner.fromJson(String source) =>
+      SliderBanner.fromMap(json.decode(source) as Map<String, dynamic>);
 }
