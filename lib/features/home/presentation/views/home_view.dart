d8dfb360a8494407c262a36adf3330e06944d9f7
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 16:34:24 +0530

    feat: create base layout for `HomeView`

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -5,8 +5,100 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('HomeView'),
+    final screenSize = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: CustomScrollView(
+        slivers: [
+          // Slider banners page view
+          SliverToBoxAdapter(
+            child: SizedBox(
+              width: screenSize.width,
+              height: 200,
+              child: PageView.builder(
+                padEnds: false,
+                itemCount: 5,
+                controller: PageController(viewportFraction: 0.8),
+                itemBuilder: (context, index) {
+                  return const Padding(
+                    padding: EdgeInsets.all(8.0),
+                    child: ColoredBox(color: Colors.red),
+                  );
+                },
+              ),
+            ),
+          ),
+
+          // Featured products
+          SliverToBoxAdapter(
+            child: SizedBox(
+              width: screenSize.width,
+              height: 150,
+              child: Column(
+                children: [
+                  const Text('Featured'),
+                  Expanded(
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemBuilder: (context, index) {
+                        return const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: SizedBox(
+                            width: 200,
+                            child: ColoredBox(
+                              color: Colors.blue,
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+          // Additional banners
+          SliverToBoxAdapter(
+            child: SizedBox(
+              width: screenSize.width,
+              height: 150,
+              child: const Padding(
+                padding: EdgeInsets.all(8.0),
+                child: ColoredBox(color: Colors.red),
+              ),
+            ),
+          ),
+
+          // Best seller products
+          const SliverToBoxAdapter(
+            child: Text('Best sellers'),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              childCount: 10,
+              (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: const [
+                      SizedBox.square(
+                        dimension: 80,
+                        child: ColoredBox(color: Colors.green),
+                      ),
+                      SizedBox(width: 10),
+                      Text('Item'),
+                    ],
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
