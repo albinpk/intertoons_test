8f513dfcce41cbaad4a55b0e3e421e26dec3fa1c
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 11:14:33 +0530

    feat: create `MyApp` widget

---
+++ lib/my_app.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Intertoons Test App',
+      home: _HomePage(),
+    );
+  }
+}
+
+class _HomePage extends StatelessWidget {
+  const _HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('HomePage')),
+    );
+  }
+}
