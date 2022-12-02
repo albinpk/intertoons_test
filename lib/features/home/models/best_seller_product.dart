47062bb28b514c74ba0ebc21c69abcef44d902cc
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-02 15:02:45 +0530

    feat: create `BestSellerProduct` class

---
+++ lib/features/home/models/best_seller_product.dart
@@ -0,0 +1,25 @@
+import 'product_base.dart';
+
+/// Best seller product model.
+class BestSellerProduct extends ProductBase {
+  const BestSellerProduct({
+    required super.id,
+    required super.name,
+    required super.sku,
+    required super.categoryId,
+    required super.categoryName,
+    required super.isVeg,
+    required super.description,
+    required super.price,
+    required super.imageUrl,
+    required super.variations,
+    required this.orderCount,
+    required this.menuStatus,
+  });
+
+  /// Order count.
+  final int orderCount;
+
+  /// Menu status.
+  final bool menuStatus;
+}
