87a890f4fa2492232eb73edbd64a2021474f023d
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 08:36:39 +0530

    feat: pull to refresh

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -19,49 +19,58 @@ class HomeView extends StatelessWidget {
       create: (context) => HomeViewCubit(
         homeRepository: context.read<HomeRepository>(),
       ),
-      child: SafeArea(
-        child: CustomScrollView(
-          slivers: [
-            // Delivery address
-            const HomeAppBar(),
+      child: Builder(
+        builder: (context) {
+          return SafeArea(
+            child: RefreshIndicator(
+              onRefresh: () async {
+                context.read<HomeViewCubit>().refresh();
+              },
+              child: CustomScrollView(
+                slivers: [
+                  // Delivery address
+                  const HomeAppBar(),
 
-            // Slider banners page view
-            SliverToBoxAdapter(
-              child: SizedBox(
-                width: screenSize.width,
-                height: 150,
-                child: const SliderBannerListView(),
+                  // Slider banners page view
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      width: screenSize.width,
+                      height: 150,
+                      child: const SliderBannerListView(),
+                    ),
+                  ),
+
+                  // Featured products
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      width: screenSize.width,
+                      height: 150,
+                      child: const FeaturedProductsList(),
+                    ),
+                  ),
+
+                  // Additional banners
+                  SliverToBoxAdapter(
+                    child: SizedBox(
+                      width: screenSize.width,
+                      height: 150,
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: ColoredBox(color: Colors.red),
+                      ),
+                    ),
+                  ),
+
+                  // Best seller products
+                  const SliverToBoxAdapter(
+                    child: Text('Best sellers'),
+                  ),
+                  const BestSellersList(),
+                ],
               ),
             ),
-
-            // Featured products
-            SliverToBoxAdapter(
-              child: SizedBox(
-                width: screenSize.width,
-                height: 150,
-                child: const FeaturedProductsList(),
-              ),
-            ),
-
-            // Additional banners
-            SliverToBoxAdapter(
-              child: SizedBox(
-                width: screenSize.width,
-                height: 150,
-                child: const Padding(
-                  padding: EdgeInsets.all(8.0),
-                  child: ColoredBox(color: Colors.red),
-                ),
-              ),
-            ),
-
-            // Best seller products
-            const SliverToBoxAdapter(
-              child: Text('Best sellers'),
-            ),
-            const BestSellersList(),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
