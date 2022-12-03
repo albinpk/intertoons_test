9e012954926d96863000a78d4dacc6bc1eaa6132
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 12:47:11 +0530

    refactor: add `additionalBanners` property to `HomeViewState`

--- lib/features/home/presentation/cubit/home_view_state.dart
+++ lib/features/home/presentation/cubit/home_view_state.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'home_view_cubit.dart';
 
 /// Home view status.
@@ -9,21 +10,32 @@ class HomeViewState {
     this.status = HomeViewStatus.initial,
     this.sliderBanners = const [],
     this.featuredProducts = const [],
+    this.additionalBanners = const [],
   });
 
+  /// HomeView status
   final HomeViewStatus status;
+
+  /// List of [SliderBanner]s.
   final List<SliderBanner> sliderBanners;
+
+  /// List of [FeaturedProduct]s.
   final List<FeaturedProduct> featuredProducts;
+
+  /// List of additional [SliderBanner]s.
+  final List<SliderBanner> additionalBanners;
 
   HomeViewState copyWith({
     HomeViewStatus? status,
     List<SliderBanner>? sliderBanners,
     List<FeaturedProduct>? featuredProducts,
+    List<SliderBanner>? additionalBanners,
   }) {
     return HomeViewState(
       status: status ?? this.status,
       sliderBanners: sliderBanners ?? this.sliderBanners,
       featuredProducts: featuredProducts ?? this.featuredProducts,
+      additionalBanners: additionalBanners ?? this.additionalBanners,
     );
   }
 }
