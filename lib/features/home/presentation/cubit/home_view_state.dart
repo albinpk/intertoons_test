0633f2d0b4e12897aca02fec63305d5948f31683
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 11:27:13 +0530

    refactor: add `featuredProducts` field to `HomeViewState`

--- lib/features/home/presentation/cubit/home_view_state.dart
+++ lib/features/home/presentation/cubit/home_view_state.dart
@@ -8,18 +8,22 @@ class HomeViewState {
   const HomeViewState({
     this.status = HomeViewStatus.initial,
     this.sliderBanners = const [],
+    this.featuredProducts = const [],
   });
 
   final HomeViewStatus status;
   final List<SliderBanner> sliderBanners;
+  final List<FeaturedProduct> featuredProducts;
 
   HomeViewState copyWith({
     HomeViewStatus? status,
     List<SliderBanner>? sliderBanners,
+    List<FeaturedProduct>? featuredProducts,
   }) {
     return HomeViewState(
       status: status ?? this.status,
       sliderBanners: sliderBanners ?? this.sliderBanners,
+      featuredProducts: featuredProducts ?? this.featuredProducts,
     );
   }
 }
