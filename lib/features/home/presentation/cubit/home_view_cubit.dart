bc8dc8f9a5f161689e8a56efb1a4dad223ba2f1e
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 11:29:41 +0530

    refactor: create `_getFeaturedProductFromJson` method in `HomeViewCubit`
    refactor: _getSliderBannerFromJson method.

--- lib/features/home/presentation/cubit/home_view_cubit.dart
+++ lib/features/home/presentation/cubit/home_view_cubit.dart
@@ -1,7 +1,10 @@
 import 'dart:developer';
+
 import 'package:flutter/foundation.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../core/types.dart';
+import '../../models/featured_product.dart';
 import '../../models/slider_banner.dart';
 import '../../repositories/home_repository.dart';
 
@@ -19,12 +22,15 @@ class HomeViewCubit extends Cubit<HomeViewState> {
   Future<void> _getHomeData() async {
     emit(state.copyWith(status: HomeViewStatus.loading));
     try {
-      final res = await _repository.fetchHomeData();
-      final banners = _getSliderBannersFromJson(res);
-      emit(state.copyWith(
-        sliderBanners: banners,
-        status: HomeViewStatus.success,
-      ));
+      final json = await _repository.fetchHomeData();
+      if (!json.containsKey('data')) throw 'No data';
+      emit(
+        state.copyWith(
+          status: HomeViewStatus.success,
+          sliderBanners: _getSliderBannersFromJson(json),
+          featuredProducts: _getFeaturedProductsFromJson(json),
+        ),
+      );
     } catch (err) {
       log(err.toString());
       emit(state.copyWith(status: HomeViewStatus.failure));
@@ -35,17 +41,30 @@ class HomeViewCubit extends Cubit<HomeViewState> {
   void refresh() => _getHomeData();
 
   /// Extract and return list of [SliderBanner]s.
-  List<SliderBanner> _getSliderBannersFromJson(Map<String, dynamic> json) {
+  List<SliderBanner> _getSliderBannersFromJson(MapData json) {
     try {
-      if (!json.containsKey('data')) throw 'No data';
-
-      if (!(json['data'] as Map<String, dynamic>)
-          .containsKey('slider_banners')) {
+      if (!(json['data'] as MapData).containsKey('slider_banners')) {
         throw 'No banners';
       }
 
       return (json['data']['slider_banners'] as List)
           .map((e) => SliderBanner.fromMap(e))
+          .toList();
+    } catch (err) {
+      log(err.toString());
+      rethrow;
+    }
+  }
+
+  /// Extract and return list of [FeaturedProduct]s.
+  List<FeaturedProduct> _getFeaturedProductsFromJson(MapData json) {
+    try {
+      if (!(json['data'] as MapData).containsKey('featured_products')) {
+        throw 'No Featured products';
+      }
+
+      return (json['data']['featured_products'] as List)
+          .map((e) => FeaturedProduct.fromMap(e))
           .toList();
     } catch (err) {
       log(err.toString());
