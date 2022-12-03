5773c13c0a765f69c2ab22f2dfd22787189b54fb
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 11:25:23 +0530

    refactor: add JSON serialization in `ProductVariation`
    change specialPrice property type to double.

--- lib/features/home/models/product_variation.dart
+++ lib/features/home/models/product_variation.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 import 'package:equatable/equatable.dart';
 
 /// Product variation mode.
@@ -19,8 +21,38 @@ class ProductVariation extends Equatable {
   final String price;
 
   /// Product variation special price.
-  final String specialPrice;
+  final double specialPrice;
 
   @override
   List<Object> get props => [id, title, price, specialPrice];
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      _kId: id,
+      _kTitle: title,
+      _kPrice: price,
+      _kSpecialPrice: specialPrice,
+    };
+  }
+
+  factory ProductVariation.fromMap(Map<String, dynamic> map) {
+    return ProductVariation(
+      id: map[_kId] as int,
+      title: map[_kTitle] as String,
+      price: map[_kPrice] as String,
+      specialPrice: map[_kSpecialPrice] is int
+          ? map[_kSpecialPrice].toDouble()
+          : double.parse(map[_kSpecialPrice]),
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory ProductVariation.fromJson(String source) =>
+      ProductVariation.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  static const _kSpecialPrice = 'special_price';
+  static const _kPrice = 'price';
+  static const _kTitle = 'title';
+  static const _kId = 'id';
 }
