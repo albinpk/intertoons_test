58de5e5fb0ce8917d2045f405a332eaa80dae59b
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 11:36:03 +0530

    feat: show featured product

--- lib/features/home/presentation/widgets/featured_products_list.dart
+++ lib/features/home/presentation/widgets/featured_products_list.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import '../../models/featured_product.dart';
+import '../cubit/home_view_cubit.dart';
 
 /// Featured products list view.
 class FeaturedProductsList extends StatelessWidget {
@@ -17,25 +22,130 @@ class FeaturedProductsList extends StatelessWidget {
           ),
         ),
         Expanded(
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index) {
-              return const Padding(
-                padding: EdgeInsets.all(8.0),
-                child: SizedBox(
-                  width: 200,
-                  child: ColoredBox(
-                    color: Colors.blue,
-                    child: Padding(
-                      padding: EdgeInsets.all(8.0),
-                    ),
+          child: BlocBuilder<HomeViewCubit, HomeViewState>(
+            builder: (context, state) {
+              if (state.status == HomeViewStatus.failure) {
+                return Center(
+                  child: Text(
+                    'An error has occurred.',
+                    style: Theme.of(context).textTheme.bodySmall,
                   ),
-                ),
+                );
+              }
+
+              final isLoading = state.status == HomeViewStatus.loading ||
+                  state.status == HomeViewStatus.initial;
+
+              return ListView.builder(
+                itemCount: isLoading ? 2 : state.featuredProducts.length,
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.symmetric(horizontal: 5),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  return _ListItem(
+                    featuredProduct:
+                        isLoading ? null : state.featuredProducts[index],
+                  );
+                },
               );
             },
           ),
         ),
       ],
+    );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  const _ListItem({
+    Key? key,
+    required this.featuredProduct,
+  }) : super(key: key);
+
+  final FeaturedProduct? featuredProduct;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(5),
+      child: AspectRatio(
+        aspectRatio: 3 / 2,
+        child: featuredProduct == null
+            ? ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: const ColoredBox(
+                  color: Colors.black12,
+                  child: Center(
+                    child: CircularProgressIndicator(
+                      color: Colors.grey,
+                      strokeWidth: 2,
+                    ),
+                  ),
+                ),
+              )
+            : Card(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                clipBehavior: Clip.antiAlias,
+                margin: EdgeInsets.zero,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Expanded(
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        fit: BoxFit.cover,
+                        image: featuredProduct!.imageUrl,
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            featuredProduct!.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(
+                                  fontWeight: FontWeight.w600,
+                                ),
+                            maxLines: 1,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                '\$${featuredProduct!.specialPrice}',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleMedium!
+                                    .copyWith(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.red,
+                                    ),
+                              ),
+                              const SizedBox(width: 10),
+                              Text(
+                                '\$${featuredProduct!.price}',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .titleSmall!
+                                    .copyWith(
+                                      color: Colors.grey,
+                                      decoration: TextDecoration.lineThrough,
+                                    ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
     );
   }
 }
