d5b40a1a905e655d937dd0353d1a86dbaff0be53
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-03 21:21:20 +0530

    feat: show categories in `MenuView`

--- lib/features/menu/presentation/views/menu_view.dart
+++ lib/features/menu/presentation/views/menu_view.dart
@@ -1,10 +1,62 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class MenuView extends StatelessWidget {
+import '../cubit/menu_view_cubit.dart';
+
+class MenuView extends StatefulWidget {
   const MenuView({super.key});
 
   @override
+  State<MenuView> createState() => _MenuViewState();
+}
+
+class _MenuViewState extends State<MenuView>
+    with AutomaticKeepAliveClientMixin {
+  @override
   Widget build(BuildContext context) {
-    return const Center(child: Text('MenuView'));
+    super.build(context);
+
+    return BlocBuilder<MenuViewCubit, MenuViewState>(
+      builder: (context, state) {
+        if (state.status == MenuViewStatus.failure) {
+          return Center(
+            child: Text(
+              'An error has occurred.',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+          );
+        }
+
+        final isLoading = state.status == MenuViewStatus.loading;
+
+        return DefaultTabController(
+          length: state.categories.length,
+          child: Scaffold(
+            appBar: AppBar(
+              title: const Text('Explore menu'),
+              backgroundColor: Colors.black87,
+              bottom: isLoading
+                  ? null
+                  : TabBar(
+                      isScrollable: true,
+                      tabs: state.categories
+                          .map((e) => Tab(child: Text(e.name)))
+                          .toList(),
+                    ),
+            ),
+            body: isLoading
+                ? const Center(child: CircularProgressIndicator())
+                : TabBarView(
+                    children: state.categories
+                        .map((e) => Center(child: Text(e.name)))
+                        .toList(),
+                  ),
+          ),
+        );
+      },
+    );
   }
+
+  @override
+  bool get wantKeepAlive => true;
 }
