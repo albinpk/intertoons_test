2ac08e86780a15cf5d5c878740d472995f64d1d4
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-04 20:22:22 +0530

    feat: show/hide `BottomCartBar` using `AnimatedAlign` widget

--- lib/core/widgets/bottom_cart_bar.dart
+++ lib/core/widgets/bottom_cart_bar.dart
@@ -12,70 +12,82 @@ class BottomCartBar extends StatelessWidget {
   Widget build(BuildContext context) {
     final textTheme = Theme.of(context).textTheme;
 
-    return ColoredBox(
-      color: Colors.black,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  BlocSelector<CartCubit, CartState, int>(
-                    selector: (state) => state.items.length,
-                    builder: (context, itemCount) {
-                      return RichText(
-                        text: TextSpan(
-                          style: textTheme.titleSmall!.copyWith(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w400,
-                          ),
-                          children: [
-                            TextSpan(
-                              text: '$itemCount ',
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
+    // To show/hide this widget using the AnimatedAlign widget
+    final isEmpty = context.select(
+      (CartCubit bloc) => bloc.state.items.isEmpty,
+    );
+
+    return AnimatedAlign(
+      alignment: Alignment.topCenter,
+      curve: Curves.easeInOut,
+      duration: const Duration(milliseconds: 200),
+      heightFactor: isEmpty ? 0 : 1,
+      child: ColoredBox(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    BlocSelector<CartCubit, CartState, int>(
+                      selector: (state) => state.items.length,
+                      builder: (context, itemCount) {
+                        return RichText(
+                          text: TextSpan(
+                            style: textTheme.titleSmall!.copyWith(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
                             ),
-                            TextSpan(
-                              text:
-                                  'item${itemCount > 1 ? 's' : ''} in the cart',
-                            )
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                  const SizedBox(height: 3),
-
-                  // Total amount
-                  RichText(
-                    text: TextSpan(
-                      style: textTheme.titleMedium!.copyWith(
-                        color: Colors.white,
-                      ),
-                      children: const [
-                        TextSpan(text: 'Total: '),
-                        TextSpan(
-                          text: '\$50.0',
-                          style: TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                            children: [
+                              TextSpan(
+                                text: '$itemCount ',
+                                style: const TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              TextSpan(
+                                text:
+                                    'item${itemCount > 1 ? 's' : ''} in the cart',
+                              )
+                            ],
+                          ),
+                        );
+                      },
                     ),
-                  ),
-                ],
-              ),
-            ),
+                    const SizedBox(height: 3),
 
-            // Goto cart button.
-            TextButton(
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.white,
+                    // Total amount
+                    RichText(
+                      text: TextSpan(
+                        style: textTheme.titleMedium!.copyWith(
+                          color: Colors.white,
+                        ),
+                        children: const [
+                          TextSpan(text: 'Total: '),
+                          TextSpan(
+                            text: '\$50.0',
+                            style: TextStyle(fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
-              onPressed: () {},
-              child: const Text("Goto Cart"),
-            ),
-          ],
+
+              // Goto cart button.
+              TextButton(
+                style: TextButton.styleFrom(
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () {},
+                child: const Text("Goto Cart"),
+              ),
+            ],
+          ),
         ),
       ),
     );
