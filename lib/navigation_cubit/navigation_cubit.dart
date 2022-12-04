bd1db8a7f179a05c5ade5d563eb4f6987012c15d
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-04 12:51:00 +0530

    refactor: create `changeTabWithData` method in `NavigationCubit`
    update NavigationState.

--- lib/navigation_cubit/navigation_cubit.dart
+++ lib/navigation_cubit/navigation_cubit.dart
@@ -10,4 +10,9 @@ class NavigationCubit extends Cubit<NavigationState> {
   void changeTab(int i) {
     emit(state.copyWith(currentTab: i));
   }
+
+  /// Change bottom navigation with data.
+  void changeTabWithData(int i, {required Object data}) {
+    emit(state.copyWith(currentTab: i, data: data));
+  }
 }
--- lib/navigation_cubit/navigation_state.dart
+++ lib/navigation_cubit/navigation_state.dart
@@ -3,19 +3,25 @@ part of 'navigation_cubit.dart';
 class NavigationState extends Equatable {
   const NavigationState({
     this.currentTab = 0,
+    this.data,
   });
 
   /// Current tab index.
   final int currentTab;
 
+  /// Data for the tab.
+  final Object? data;
+
   @override
-  List<Object> get props => [currentTab];
+  List<Object?> get props => [currentTab, data];
 
   NavigationState copyWith({
     int? currentTab,
+    Object? data,
   }) {
     return NavigationState(
       currentTab: currentTab ?? this.currentTab,
+      data: data,
     );
   }
 }
