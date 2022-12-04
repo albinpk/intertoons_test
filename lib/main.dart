33dc737ae4a580a0aba591b6f71e0947c0054b98
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-04 19:49:33 +0530

    refactor: setup storage for HydratedBloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
 
 import 'my_app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  try {
+    HydratedBloc.storage = await HydratedStorage.build(
+      storageDirectory: await getTemporaryDirectory(),
+    );
+  } catch (err) {
+    log("Can't build HydratedBloc storage", error: err);
+  }
+
   runApp(const MyApp());
 }
