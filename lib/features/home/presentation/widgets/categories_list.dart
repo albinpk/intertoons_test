af6b5c450b5142ef2020eaa59900b82d2ba36d07
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-04 10:10:20 +0530

    feat: add `InkWell` to category item

--- lib/features/home/presentation/widgets/categories_list.dart
+++ lib/features/home/presentation/widgets/categories_list.dart
@@ -10,6 +10,8 @@ class CategoriesList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
@@ -20,7 +22,7 @@ class CategoriesList extends StatelessWidget {
           ),
           child: Text(
             'Explore menu',
-            style: Theme.of(context).textTheme.titleLarge,
+            style: textTheme.titleLarge,
           ),
         ),
 
@@ -32,7 +34,7 @@ class CategoriesList extends StatelessWidget {
                 return Center(
                   child: Text(
                     'An error has occurred.',
-                    style: Theme.of(context).textTheme.bodySmall,
+                    style: textTheme.bodySmall,
                   ),
                 );
               }
@@ -90,28 +92,47 @@ class _ListItem extends StatelessWidget {
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
                 ),
+                clipBehavior: Clip.antiAlias,
                 margin: EdgeInsets.zero,
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Expanded(
-                        child: FadeInImage.memoryNetwork(
-                          placeholder: kTransparentImage,
-                          fit: BoxFit.fitHeight,
-                          image: category!.imageUrl,
-                        ),
+                // Ink.image with FadeInImage not working.
+                // Using Stack to place InkWell on top of the card.
+                child: Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    // Content
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        children: [
+                          Expanded(
+                            child: FadeInImage.memoryNetwork(
+                              placeholder: kTransparentImage,
+                              image: category!.imageUrl,
+                              fit: BoxFit.fitHeight,
+                            ),
+                          ),
+                          const SizedBox(height: 8),
+                          Text(
+                            category!.name,
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleMedium!
+                                .copyWith(fontWeight: FontWeight.w500),
+                          ),
+                        ],
                       ),
-                      const SizedBox(height: 8),
-                      Text(
-                        category!.name,
-                        style:
-                            Theme.of(context).textTheme.titleMedium!.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                ),
+                    ),
+
+                    // Splash
+                    Material(
+                      color: Colors.transparent,
+                      child: InkWell(
+                        onTap: () {},
+                        highlightColor: Colors.grey.withOpacity(0.1),
+                        splashColor: Colors.grey.withOpacity(0.1),
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
       ),
