5bb7ad66c433c40abb7189919f66a86f0286861a
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-05 13:09:15 +0530

    feat: show cart items in `OrderSummaryScreen`

--- lib/core/screens/order_summary_screen.dart
+++ lib/core/screens/order_summary_screen.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:transparent_image/transparent_image.dart';
 
+import '../../features/home/presentation/cubit/home_view_cubit.dart';
+import '../constants.dart';
 import '../cubit/cart_cubit.dart';
+import '../models/cart_item_model.dart';
+import '../models/featured_product_model.dart';
+import '../widgets/add_to_cart_button.dart';
 
 class OrderSummaryScreen extends StatelessWidget {
   const OrderSummaryScreen({super.key});
@@ -16,13 +22,113 @@ class OrderSummaryScreen extends StatelessWidget {
       body: BlocBuilder<CartCubit, CartState>(
         builder: (context, state) {
           return ListView.builder(
+            itemExtent: 150,
             itemCount: state.items.length,
             itemBuilder: (context, index) {
-              return Text('Cart item $index');
+              return _ListItem(
+                item: state.items[index],
+              );
             },
           );
         },
       ),
     );
+  }
+}
+
+class _ListItem extends StatelessWidget {
+  const _ListItem({
+    Key? key,
+    required this.item,
+  }) : super(key: key);
+
+  final CartItem item;
+
+  @override
+  Widget build(BuildContext context) {
+    final product = context.select(
+      (HomeViewCubit bloc) => bloc.state.featuredProducts.singleWhere(
+        (p) => p.id == item.productId,
+      ),
+    );
+
+    return Card(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            // Image and name
+            Expanded(
+              child: Row(
+                children: [
+                  // Image
+                  AspectRatio(
+                    aspectRatio: 3 / 2,
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: FadeInImage.memoryNetwork(
+                        placeholder: kTransparentImage,
+                        fit: BoxFit.cover,
+                        image: product.imageUrl,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(width: 10),
+
+                  // Name
+                  Expanded(
+                    child: Text(
+                      product.name,
+                      style: Theme.of(context).textTheme.titleLarge,
+                    ),
+                  ),
+
+                  // Remove button
+                  IconButton(
+                    onPressed: () {
+                      context.read<CartCubit>().deleteFromCart(product);
+                    },
+                    color: Colors.red,
+                    icon: const Icon(Icons.delete),
+                  ),
+                ],
+              ),
+            ),
+
+            Expanded(
+              child: Row(
+                children: [
+                  // Quantity button
+                  AddToCartButton(
+                    product: product,
+                    minimumSize: const Size(90, 30),
+                  ),
+                  const SizedBox(width: 50),
+
+                  // Total price
+                  Expanded(
+                    child: Text(
+                      _price(product),
+                      textAlign: TextAlign.end,
+                      style: Theme.of(context).textTheme.titleMedium!.copyWith(
+                            fontWeight: FontWeight.bold,
+                            color: primaryColor,
+                          ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  String _price(FeaturedProduct product) {
+    final productPrice =
+        product.specialPrice == 0 ? product.price : product.specialPrice;
+    final totalPrice = item.productCount * productPrice;
+    return '\$$totalPrice';
   }
 }
