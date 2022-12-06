a49c2605ebe605ab32655ef92a3b721e8a538603
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-06 10:51:06 +0530

    feat: show all products in `ProductsView`

--- lib/features/home/presentation/views/products_view.dart
+++ lib/features/home/presentation/views/products_view.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../../../core/cubit/products_cubit.dart';
+import '../widgets/product_tile.dart';
 
 class ProductsView extends StatelessWidget {
   const ProductsView({super.key});
@@ -10,8 +14,27 @@ class ProductsView extends StatelessWidget {
         backgroundColor: Colors.black87,
         title: const Text('Products'),
       ),
-      body: const Center(
-        child: Text('Products'),
+      body: BlocBuilder<ProductsCubit, ProductsState>(
+        builder: (context, state) {
+          if (state.status == ProductsStatus.failure) {
+            return const Center(child: Text('An error has occurred!'));
+          }
+
+          final isLoading = state.status == ProductsStatus.loading ||
+              state.status == ProductsStatus.initial;
+
+          return ListView.builder(
+            itemCount: isLoading ? 3 : state.products.length,
+            itemExtent: 90,
+            padding: const EdgeInsets.only(bottom: 30, top: 10),
+            physics: const BouncingScrollPhysics(),
+            itemBuilder: (context, index) {
+              return ProductTile(
+                product: isLoading ? null : state.products[index],
+              );
+            },
+          );
+        },
       ),
     );
   }
