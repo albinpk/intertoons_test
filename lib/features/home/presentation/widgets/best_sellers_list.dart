041c49f5d0a2a6fb4910dee5a74958e9a85d5585
Albin P K <56157868+albinpk@users.noreply.github.com>
2022-12-06 10:46:01 +0530

    refactor: extract `ProductTile` widget

--- lib/features/home/presentation/widgets/best_sellers_list.dart
+++ lib/features/home/presentation/widgets/best_sellers_list.dart
@@ -1,12 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:transparent_image/transparent_image.dart';
 
-import '../../../../core/constants.dart';
-import '../../../../core/models/best_seller_product_model.dart';
-import '../../../../core/widgets/add_to_cart_button.dart';
 import '../cubit/home_view_cubit.dart';
-import 'veg_symbol.dart';
+import 'product_tile.dart';
 
 /// Best seller products.
 class BestSellersList extends StatelessWidget {
@@ -38,116 +34,13 @@ class BestSellersList extends StatelessWidget {
           delegate: SliverChildBuilderDelegate(
             childCount: isLoading ? 3 : state.bestSellerProducts.length,
             (context, index) {
-              return _ListItem(
+              return ProductTile(
                 product: isLoading ? null : state.bestSellerProducts[index],
               );
             },
           ),
         );
       },
-    );
-  }
-}
-
-class _ListItem extends StatelessWidget {
-  const _ListItem({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
-
-  final BestSellerProduct? product;
-
-  static final text = ClipRRect(
-    borderRadius: BorderRadius.circular(8),
-    child: const ColoredBox(color: Colors.black12),
-  );
-
-  @override
-  Widget build(BuildContext context) {
-    final textTheme = Theme.of(context).textTheme;
-
-    return Padding(
-      padding: const EdgeInsets.all(10).copyWith(top: 0),
-      child: product == null
-          ? text
-          : Row(
-              children: [
-                // Product image
-                SizedBox(
-                  width: 100,
-                  height: 80,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: FadeInImage.memoryNetwork(
-                      placeholder: kTransparentImage,
-                      image: product!.imageUrl,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                // const SizedBox(width: 10),
-
-                // Product details
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.all(5).copyWith(left: 10),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Row(
-                          children: [
-                            VegSymbol(isVeg: product!.isVeg),
-                            const SizedBox(width: 8),
-
-                            // Product name
-                            Expanded(
-                              child: Text(
-                                product!.name,
-                                style: textTheme.titleMedium!.copyWith(
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                overflow: TextOverflow.ellipsis,
-                                maxLines: 1,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-
-                        // Product description
-                        Expanded(
-                          child: Text(
-                            product!.description,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        ),
-
-                        // ADD button and price
-                        Row(
-                          children: [
-                            AddToCartButton(
-                              product: product!,
-                              minimumSize: const Size(50, 25),
-                            ),
-                            const SizedBox(width: 10),
-
-                            // Product price
-                            Text(
-                              '\$${product!.price}',
-                              style: textTheme.titleSmall!.copyWith(
-                                color: primaryColor,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
     );
   }
 }
---
+++ lib/features/home/presentation/widgets/product_tile.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:transparent_image/transparent_image.dart';
+
+import '../../../../core/constants.dart';
+import '../../../../core/models/product_model.dart';
+import '../../../../core/widgets/add_to_cart_button.dart';
+import 'veg_symbol.dart';
+
+class ProductTile extends StatelessWidget {
+  const ProductTile({
+    super.key,
+    required this.product,
+  });
+
+  final Product? product;
+
+  static final text = ClipRRect(
+    borderRadius: BorderRadius.circular(8),
+    child: const ColoredBox(color: Colors.black12),
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
+    return Padding(
+      padding: const EdgeInsets.all(10).copyWith(top: 0),
+      child: product == null
+          ? text
+          : Row(
+              children: [
+                // Product image
+                SizedBox(
+                  width: 100,
+                  height: 80,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(8),
+                    child: FadeInImage.memoryNetwork(
+                      placeholder: kTransparentImage,
+                      image: product!.imageUrl,
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+                // const SizedBox(width: 10),
+
+                // Product details
+                Expanded(
+                  child: Padding(
+                    padding: const EdgeInsets.all(5).copyWith(left: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            VegSymbol(isVeg: product!.isVeg),
+                            const SizedBox(width: 8),
+
+                            // Product name
+                            Expanded(
+                              child: Text(
+                                product!.name,
+                                style: textTheme.titleMedium!.copyWith(
+                                  fontWeight: FontWeight.w500,
+                                ),
+                                overflow: TextOverflow.ellipsis,
+                                maxLines: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 5),
+
+                        // Product description
+                        Expanded(
+                          child: Text(
+                            product!.description,
+                            maxLines: 1,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+
+                        // ADD button and price
+                        Row(
+                          children: [
+                            AddToCartButton(
+                              product: product!,
+                              minimumSize: const Size(50, 25),
+                            ),
+                            const SizedBox(width: 10),
+
+                            // TODO: special price
+                            // Product price
+                            Text(
+                              '\$${product!.price}',
+                              style: textTheme.titleSmall!.copyWith(
+                                color: primaryColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+    );
+  }
+}
